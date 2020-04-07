de546b2b72dc36ad0af74744f8c6842fc0371373
Krolag <dawidpaweloszek99@gmail.com>
2020-04-07 11:26:21 +0200

    Add files via upload

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -17,43 +17,104 @@ def randVector(rows, columns):
 
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
+
+def dsigmoid(y):
+    # return sigmoid(x) * (1 - sigmoid(x))
+    return y * (1 - y)
     
 def map(this_arr, function):
     size = np.shape(this_arr)
+    result = this_arr
     for i in range(size[0]):
         for j in range(size[1]):
             value = this_arr[i][j]
-            this_arr[i][j] = function(value)
+            result[i][j] = function(value)
+    return result
 
 class NeuralNetwork:
     def __init__(self, numI, numH, numO, is_bias):
         self.input_nodes = numI
         self.hidden_nodes = numH
         self.output_nodes = numO
+        self.learing_rate = 0.1
         self.bias_active = is_bias
         self.bias_h = np.full((self.hidden_nodes, 1), 1)
         self.bias_o = np.full((self.output_nodes, 1), 1)
         self.weigts_ih = randVector(int(self.hidden_nodes), int(self.input_nodes))
         self.weigts_ho = randVector(int(self.output_nodes), int(self.hidden_nodes))
+        self.function = np.array([])
 
-    def feedForward(self, input_array):
+    def predict(self, input_array):
         # Generating the Hidden Outputs
         inputs = input_array
         hidden = np.dot(self.weigts_ih, inputs)
-        hidden = np.array(hidden)
-        if (self.bias_active == True):
-            hidden = hidden + self.bias_h
-        map(hidden, sigmoid)
+        hidden += self.bias_h * 1.0
 
-        # Generating the Output's Output
+        # Activation fuction
+        hidden = map(hidden, sigmoid)
+
+        # Generating the output's output
         output = np.dot(self.weigts_ho, hidden)
-        output = np.array(output)
-        if (self.bias_active == True):
-            output = output + self.bias_o
-        map(output, sigmoid)
+        output += self.bias_o * 1.0
+        output = map(output, sigmoid)
 
-        # Sendig it back to the caller
+        # Sending back to the caller
         return output
 
-    def train(self, inputs, answer):
-        return 0
+    def train(self, input_array, targets_array):
+        # Generating the Hidden Outputs
+        inputs = input_array
+        hidden = np.dot(self.weigts_ih, inputs)
+        if (self.bias_active == True):
+            hidden += self.bias_h * 1.0
+        hidden = map(hidden, sigmoid)
+
+        # Generating the Output's Output
+        outputs = np.dot(self.weigts_ho, hidden)
+        if (self.bias_active == True):
+            outputs += self.bias_o * 1.0
+        outputs = map(outputs, sigmoid)
+
+        # Copy targets_array to targets
+        targets = targets_array
+
+        # Calculate the output's error
+        # ERROR = TARGETS - OUTPUTS
+        output_errors = targets - outputs
+        output_error_power = output_errors * output_errors
+        self.function = np.append(self.function, np.average(output_error_power))
+
+        # Calculate the gradient
+        gradients = map(outputs, dsigmoid)
+        gradients = np.multiply(gradients, output_errors)
+        gradients = np.multiply(gradients, self.learing_rate)
+
+        # Calculate deltas
+        hidden_T = np.transpose(hidden)
+        weight_ho_deltas = np.dot(gradients, hidden_T)
+
+        # Adjusting the weights by deltas
+        self.weigts_ho += weight_ho_deltas
+        # Adjust the bias by its deltas (which is just the gradients)
+        self.bias_o = self.bias_o * 1.0 + gradients
+
+        # Calculate the hidden layer errors
+        who_T = np.transpose(self.weigts_ho)
+        hidden_errors = np.dot(who_T, output_errors)
+
+        # Calculate hidden gradient
+        hidden_gradient = map(hidden, dsigmoid)
+        hidden_gradient = np.multiply(hidden_gradient, hidden_errors)
+        hidden_gradient = np.multiply(hidden_gradient, self.learing_rate)
+
+        # Calculate input->hidden deltas
+        inputs_T = np.transpose(inputs)
+        weight_ih_deltas = np.dot(hidden_gradient, inputs_T)
+
+        # Adjusting the weights by deltas
+        self.weigts_ih += weight_ih_deltas
+        # Adjust the bias by its deltas (which is just the gradients)
+        self.bias_h = self.bias_h * 1.0 + hidden_gradient
+
+        # Sending it back to the caller
+        # return output_errors + 1
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 from NeuralNetwork import NeuralNetwork
 
 if __name__ == "__main__":
     hidden_neurons = input("How many neurons in hidden layer? (1 - 3):\t")
-    is_bias =  input("With or without bias? (0 - 1):\t")
+    is_bias = input("With or without bias? (0 - 1):\t")
     if (is_bias == 0):
         is_bias = False
     else:
@@ -11,6 +13,29 @@ if __name__ == "__main__":
 
     network = NeuralNetwork(4, int(hidden_neurons), 4, is_bias)
     training_input = np.genfromtxt("training_input.txt")
+    training_output = np.genfromtxt("training_input.txt")
 
     # TESTING
-    print(network.feedForward(training_input))
+    start_time = time.time()
+    for i in range(2500):
+        network.train(training_input, training_output)
+    delta_time = (str)("--- %s seconds ---" % (time.time() - start_time))
+    output = (network.predict(training_input))
+
+    result = ''
+    for i in range(4):
+        for j in range(4):
+            if j == 3:
+                result = result + str(output[i][j])[:12] + "\\" + '\\'
+            else:
+                result = result + str(output[i][j])[:12] + " & "
+        result += '\n'
+
+    print(result)
+    plt.title(delta_time)
+    plt.grid()
+    plt.xlabel("Epoki")
+    plt.ylabel("Błąd")
+    plt.plot(network.function)
+    plt.show()
+    
